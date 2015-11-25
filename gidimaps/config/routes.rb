8b53df269645286421fecfde4c97d04df901c031
edzye101 <edzye101@gmail.com>
2015-11-25 22:06:54 +0000

    added heroku

--- gidimaps/config/routes.rb
+++ gidimaps/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
     root 'application#countdown'
-    post 'email' => 'application#email'
+    post 'email/' => 'firstusers#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
