60e136efe7b21ea2b5f3cba410a07f841d6a6c6e
edzye101 <edzye101@gmail.com>
2015-11-26 11:38:00 +0000

    changed a lot of the heroku config still hasnt worked

--- gidimaps/config/routes.rb
+++ gidimaps/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'application#countdown'
+  # root :to => 'application#countdown'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
